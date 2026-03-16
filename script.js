785bb37cc1db879273f36b5f8d41c3cb1a1ce259
Vedant Jadhav <121371485+vedant2863@users.noreply.github.com>
2026-03-16 08:15:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+  if (year % 400 === 0) {
+    return 366;
+  } else if (year % 100 === 0) {
+    return 365;
+  } else if (year % 4 === 0) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
